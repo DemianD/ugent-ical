7e91ac7bde2166dc659315bc648ed3c6b53e77d2
Demian Dekoninck <demian@live.be>
2018-09-21 18:33:39 +0200

    extra filters

--- app/Console/Commands/FetchUGentCalendar.php
+++ app/Console/Commands/FetchUGentCalendar.php
@@ -9,7 +9,8 @@ use Eluceo\iCal\Component\Event;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 
-class FetchUGentCalendar extends Command {
+class FetchUGentCalendar extends Command
+{
     
     /**
      * The name and signature of the console command.
@@ -54,9 +55,30 @@ class FetchUGentCalendar extends Command {
             ->setMethod(Calendar::METHOD_PUBLISH);
         
         $this->UGentCalendar->getEventsForAcademicYear(2018)
-            // ->filter(function ($event) {
-            //     return !data_get($event, 'groep') || in_array(config('custom.ugent.group'), $event->groep);
-            // })
+            ->filter(function ($event) {
+                if ($event->naam === 'Fysica: mechanica, optica en moderne fysica' && $event->datum->dayOfWeek === 1) {
+                    return false;
+                }
+
+                if ($event->naam === 'Wiskunde A' && $event->datum->dayOfWeek === 2) {
+                    return false;
+                }
+
+                if ($event->naam === 'Fysica: mechanica, optica en moderne fysica' && $event->datum->dayOfWeek === 3 && $event->beginuur->hour === 13) {
+                    return false;
+                }
+
+                if ($event->naam === 'Gevorderde algoritmen' && $event->datum->dayOfWeek === 4 && $event->beginuur->hour === 8) {
+                    return false;
+                }
+
+                if ($event->naam === 'Fysica: mechanica, optica en moderne fysica' && $event->datum->dayOfWeek === 5) {
+                    return false;
+                }
+
+                return true;
+                // return !data_get($event, 'groep') || in_array(config('custom.ugent.group'), $event->groep);
+            })
             ->each(function ($event) use ($calendar) {
                 $this->addEventToCalendar($calendar, $event);
             });
